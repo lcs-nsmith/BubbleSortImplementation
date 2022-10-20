96b6162409df87cd38134bc4d616d39ab6f0e042
Nathan Smith <nsmith@lcs.on.ca>
2022-10-20 11:49:32 -0400

    switched the population method of data set  to random numbers

--- BubbleSortImplementation/main.swift
+++ BubbleSortImplementation/main.swift
@@ -10,10 +10,10 @@ import Foundation
 // Create an empty list (array)
 var dataSet: [Int] = []
 
-// Populate the list
-dataSet.append(Int.random(in: 1...100))
-dataSet.append(Int.random(in: 1...100))
-dataSet.append(Int.random(in: 1...100))
+//Populate the list
+for _ in 1...10 {
+    dataSet.append(Int.random(in: 1...100))
+}
 
 // Print the list
 print(dump(dataSet))
