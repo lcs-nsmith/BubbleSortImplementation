fd928a6bca3f277e3c72ae5dfaedc6a9d324cb6c
Nathan Smith <nsmith@lcs.on.ca>
2022-10-21 14:25:54 -0400

    improved algorithm so that program exits when It has finished sorting (no more swaps)

--- BubbleSortImplementation/main.swift
+++ BubbleSortImplementation/main.swift
@@ -21,6 +21,16 @@ var dataSet: [Int] = []
 for _ in 1...10 {
     dataSet.append(Int.random(in: 1...100))
 }
+//dataSet.append(1)
+//dataSet.append(2)
+//dataSet.append(3)
+//dataSet.append(4)
+//dataSet.append(5)
+//dataSet.append(6)
+//dataSet.append(7)
+//dataSet.append(8)
+//dataSet.append(9)
+//dataSet.append(10)
 
 // Print the list
 print("Unsorted:")
@@ -31,9 +41,10 @@ waitForUserInput()
 // (however many times there are elements in the array)
 for i in 0..<dataSet.count {
     
+    var shouldBreak: Bool = true
     
     // One pass through the array to float the highest number to the end
-    for j in 0..<dataSet.count - 1 {
+    for j in 0..<dataSet.count - 1 - i {
         
         // Compare left to right value
         print("Comparison \(j+1)...", terminator: "")
@@ -45,17 +56,25 @@ for i in 0..<dataSet.count {
             dataSet[j] = dataSet[j + 1]
             dataSet[j + 1] = virtualRandomAccessMemory
             print("values were swapped", terminator: "")
+            shouldBreak = false
         }
         print("")
     }
+    
+//    if(shouldBreak) {
+//        break
+//    }
+        if shouldBreak == true {
+            exit(1)
+        }
     
     //print the array after passx   x
     print("Array after \(i + 1) passes")
     print(dataSet)
     waitForUserInput()
     
+
+
 }
 
-///
-///
-///
+/// Somehow I have to make the algorithm stop after a pass makes zero
